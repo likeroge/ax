40645c023afbdc58cae8c142172294e983b09588
Egor Puchkov <egor.puchkov@volga-dnepr.com>
2026-02-27 11:48:14 +0400

    first commit

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ async fn main() {
         .route("/api/json", get(handlers::api::get_json))
         .fallback_service(ServeDir::new("static"));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let tcp = TcpListener::bind(&addr).await.expect("Bind ip error");
 
     println!("Hello, world!");
