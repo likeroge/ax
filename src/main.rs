423a5d8c94dc75340364b28e04400d1415a8504a
likeroge <likeroge@gmail.com>
2026-02-28 14:22:37 +0400

    updated code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod errors;
 mod handlers;
 mod responses;
+mod template_structs;
 // mod template_structs;
 
 use std::net::SocketAddr;
@@ -17,12 +18,12 @@ async fn main() {
     // let swagger_ui = SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi());
     let router = Router::new()
         // .merge(swagger_ui)
-        // .route("/", get(handlers::template::html_template))
-        // .route("/users", get(handlers::template::users_page))
+        .route("/", get(handlers::template::html_template))
+        .route("/users", get(handlers::template::users_page))
         .route("/api/load", get(handlers::api::load_json_placeholder))
         .route("/api/posts/{id}", get(handlers::api::get_post_data))
         .route("/api/json", get(handlers::api::get_json))
-        .fallback_service(ServeDir::new("static"));
+        .fallback_service(ServeDir::new("./static"));
 
     // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
