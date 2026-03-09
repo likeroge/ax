c63d911849e0cbd6a0e81ae24f0761657fa374a4
likeroge <likeroge@gmail.com>
2026-03-09 10:36:18 +0400

    new route

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,32 @@ use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 use crate::app_router::AppRouter;
+
+// use crate::app_router::AppRouter;
 // use utoipa::OpenApi;
 // use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
 async fn main() {
+    let r = AppRouter::default();
     // let router = Router::new().route("/", get(async || "Hello world"));
-    let router = Router::new()
-        // .merge(swagger_ui)
-        .route("/", get(handlers::template::html_template))
-        .route("/users", get(handlers::template::users_page))
-        .route("/hello", get(handlers::template::hello_world))
-        .route("/user-form", get(handlers::template::user_form))
-        .route("/api/load", get(handlers::api::load_json_placeholder))
-        .route("/api/posts/{id}", get(handlers::api::get_post_data))
-        .route("/api/json", get(handlers::api::get_json))
-        .route("/api/create-user", post(handlers::api::create_user))
-        .fallback_service(ServeDir::new("./static"));
-
-    // let router = AppRouter::default().router;
+    // let router = Router::new()
+    //     .route("/", get(handlers::template::html_template))
+    //     .route("/users", get(handlers::template::users_page))
+    //     .route("/hello", get(handlers::template::hello_world))
+    //     .route("/user-form", get(handlers::template::user_form))
+    //     .route("/api/load", get(handlers::api::load_json_placeholder))
+    //     .route("/api/posts/{id}", get(handlers::api::get_post_data))
+    //     .route("/api/json", get(handlers::api::get_json))
+    //     .route("/api/create-user", post(handlers::api::create_user))
+    //     .fallback_service(ServeDir::new("./static"));
     // let swagger_ui = SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi());
     // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let tcp = TcpListener::bind(&addr).await.expect("Bind ip error");
 
     println!("Server started!");
-    axum::serve(tcp, router).await.expect("Error start server");
+    axum::serve(tcp, r.router)
+        .await
+        .expect("Error start server");
 }
